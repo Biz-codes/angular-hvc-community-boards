d4e1dac3f2437d13c6a73246f2488411443860f1
Elizabeth Hight <72680779+Biz-codes@users.noreply.github.com>
2022-08-01 17:23:29 -0400

    pasted in html wireframes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BulletinBoardComponent } from './bulletin-board/bulletin-board.component';
+import { DirectoryComponent } from './directory/directory.component';
+import { HubComponent } from './hub/hub.component';
+import { NewListingComponent } from './new-listing/new-listing.component';
+import { PendingComponent } from './pending/pending.component';
+import { ReportComponent } from './report/report.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BulletinBoardComponent,
+    DirectoryComponent,
+    HubComponent,
+    NewListingComponent,
+    PendingComponent,
+    ReportComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule

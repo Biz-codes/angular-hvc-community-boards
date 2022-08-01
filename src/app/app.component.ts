b8a707821bde4b8f475f30eef60572911da849d8
Elizabeth Hight <72680779+Biz-codes@users.noreply.github.com>
2022-08-01 16:42:57 -0400

    added title and basic css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-hvc-community-boards';
+  title = 'HVC Community Boards';
 }
